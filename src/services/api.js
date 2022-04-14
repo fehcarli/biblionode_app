821a4fbb2b431d8b06bec9f92acb96d711585852
Felipe Carli <lipecarli@gmail.com>
2022-04-14 00:01:24 -0300

    ajustes de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 import axios from "axios";
 // import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api"
+  baseURL: process.env.BASE_URL || 'http://localhost:4000/api'
 });
 
 // api.interceptors.request.use(async config => {
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,11 @@
-export const TOKEN_KEY = "";
+export const TOKEN_KEY = 'JWT';
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
+
 export const login = token => {
   localStorage.setItem(TOKEN_KEY, token);
 };
+
 export const logout = () => {
   localStorage.removeItem(TOKEN_KEY);
 };
